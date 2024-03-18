f64ddc56a42b861ba0859c96713ec0706b901fc2
Trantorian <eliot.code@proton.me>
2024-03-18 17:59:40 +0000

    feat(storage_root): set up storage root test

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,137 @@
-use bitvec::view::BitView;
-use bonsai_trie::{
-    databases::{create_rocks_db, RocksDB, RocksDBConfig},
-    id::BasicIdBuilder,
-    BonsaiStorage, BonsaiStorageConfig,
-};
-use starknet_types_core::{felt::Felt, hash::Pedersen};
+use std::collections::HashMap;
+use std::time::Duration;
 
-pub fn main() {
-    struct ContractState {
-        address: &'static str,
-        state_hash: &'static str,
+use anyhow::anyhow;
+use bitvec::view::BitView;
+use bonsai_trie::databases::{create_rocks_db, RocksDBConfig};
+use bonsai_trie::id::BasicIdBuilder;
+use bonsai_trie::BonsaiStorageConfig;
+use bonsai_trie::{databases::RocksDB, BonsaiStorage};
+use indicatif::{ProgressBar, ProgressStyle};
+use lazy_static::lazy_static;
+use starknet::core::types::FieldElement;
+use starknet::providers::{
+    sequencer::models::{
+        state_update::StorageDiff,
+        BlockId::{self},
+        StateUpdate,
+    },
+    SequencerGatewayProvider,
+};
+use starknet_types_core::felt::Felt;
+use starknet_types_core::hash::Pedersen;
+use tempfile::tempdir;
+use tokio::sync::RwLock;
+
+lazy_static! {
+    static ref CONTRACT_STORAGE: RwLock<HashMap<FieldElement, RwLock<HashMap<FieldElement, FieldElement>>>> =
+        RwLock::new(HashMap::new());
+}
+
+const IDENTIFIER: &[u8; 10] = b"0xcontract";
+
+#[tokio::main]
+async fn main() {
+    let provider = SequencerGatewayProvider::starknet_alpha_mainnet();
+
+    // Change this to update the range of blocks to test
+    // NOTE: This should contain the block at which `contract_address` was defined
+    let block_range = 0..400;
+
+    // The contract to watch
+    let contract_address = FieldElement::from_hex_be(
+        "0x020cfa74ee3564b4cd5435cdace0f9c4d43b939620e4a0bb5076105df0a626c6",
+    )
+    .unwrap();
+
+    // ohhh... pretty 👀
+    let bar = ProgressBar::new(block_range.end - block_range.start);
+    bar.set_style(
+        ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] {wide_bar:.cyan/blue} {pos:>7}/{len:7} {msg}")
+            .unwrap(),
+    );
+    bar.println(format!("📜 checking for contract {contract_address:#x}"));
+
+    for i in block_range {
+        bar.inc(1);
+
+        let state_update = get_state_update(&provider, i).await.unwrap();
+        if let Some(storage_updates) = state_update.state_diff.storage_diffs.get(&contract_address)
+        {
+            bar.println(format!("🧱 block {i}"));
+            save_storage_update(contract_address, storage_updates).await;
+
+            let storage_root = storage_root(contract_address, &bar).await;
+            bar.println(format!("🌳 storage root: {storage_root:#064x}"));
+        }
     }
 
-    let tempdir = tempfile::tempdir().unwrap();
+    bar.finish();
+}
+
+async fn get_state_update(
+    provider: &SequencerGatewayProvider,
+    i: u64,
+) -> anyhow::Result<StateUpdate> {
+    let mut retries = 15;
+
+    while retries > 0 {
+        if let Ok(state_update) = provider.get_state_update(BlockId::Number(i)).await {
+            return Ok(state_update);
+        }
+
+        retries -= 1;
+        tokio::time::sleep(Duration::from_secs(5)).await;
+    }
+
+    Err(anyhow!("Failed to retrieve state update for block {i}"))
+}
+
+async fn save_storage_update(contract_address: FieldElement, storage_updates: &[StorageDiff]) {
+    let storage_new: HashMap<FieldElement, FieldElement> = storage_updates
+        .iter()
+        .map(|StorageDiff { key, value }| (*key, *value))
+        .collect();
+
+    let mut contract_storage = CONTRACT_STORAGE.write().await;
+
+    match contract_storage.get(&contract_address) {
+        Some(storage_old) => {
+            storage_old.write().await.extend(storage_new);
+        }
+        None => {
+            contract_storage.insert(contract_address, RwLock::new(storage_new));
+        }
+    };
+}
+
+async fn storage_root(contract_address: FieldElement, bar: &ProgressBar) -> Felt {
+    let tempdir = tempdir().unwrap();
     let db = create_rocks_db(tempdir.path()).unwrap();
     let config = BonsaiStorageConfig::default();
     let mut bonsai_storage: BonsaiStorage<_, _, Pedersen> =
         BonsaiStorage::new(RocksDB::new(&db, RocksDBConfig::default()), config).unwrap();
 
-    let mut id_builder = BasicIdBuilder::new();
+    let contract_storage = CONTRACT_STORAGE.read().await;
+    let contract_storage = contract_storage.get(&contract_address).unwrap();
 
-    // This is an exerpt of the storage for contract
-    // 0x020cfa74ee3564b4cd5435cdace0f9c4d43b939620e4a0bb5076105df0a626c6 at block 0
-    let contract_states = vec![
-        ContractState {
-            address: "0x0000000000000000000000000000000000000000000000000000000000000005",
-            state_hash: "0x000000000000000000000000000000000000000000000000000000000000022b",
-        },
-        ContractState {
-            address: "0x0313ad57fdf765addc71329abf8d74ac2bce6d46da8c2b9b82255a5076620300",
-            state_hash: "0x04e7e989d58a17cd279eca440c5eaa829efb6f9967aaad89022acbe644c39b36",
-        },
-        // This seems to be what is causing the problem in case of double insertions.
-        // Other value are fine
-        ContractState {
-            address: "0x313ad57fdf765addc71329abf8d74ac2bce6d46da8c2b9b82255a5076620301",
-            state_hash: "0x453ae0c9610197b18b13645c44d3d0a407083d96562e8752aab3fab616cecb0",
-        },
-        ContractState {
-            address: "0x05aee31408163292105d875070f98cb48275b8c87e80380b78d30647e05854d5",
-            state_hash: "0x00000000000000000000000000000000000000000000000000000000000007e5",
-        },
-        ContractState {
-            address: "0x06cf6c2f36d36b08e591e4489e92ca882bb67b9c39a3afccf011972a8de467f0",
-            state_hash: "0x07ab344d88124307c07b56f6c59c12f4543e9c96398727854a322dea82c73240",
-        },
-    ];
+    for (key, value) in contract_storage.read().await.iter() {
+        bar.println(format!("🔑 {key:#x} -> {value:#x}"));
 
-    for contract_state in contract_states {
-        let key = contract_state.address;
-        let value = contract_state.state_hash;
-
-        let key = Felt::from_hex(key).unwrap();
-        let bitkey = key.to_bytes_be().view_bits()[5..].to_bitvec();
-        let value = Felt::from_hex(value).unwrap();
+        let key = key.to_bytes_be().view_bits()[5..].to_owned();
+        let value = Felt::from_bytes_be(&value.to_bytes_be());
 
         bonsai_storage
-            .insert(&bitkey, &value)
-            .expect("Failed to insert storage update into trie");
-
-        // fails here for key 0x313ad57fdf765addc71329abf8d74ac2bce6d46da8c2b9b82255a5076620301
-        // and value 0x453ae0c9610197b18b13645c44d3d0a407083d96562e8752aab3fab616cecb0
-        bonsai_storage.insert(&bitkey, &value).expect(&format!(
-            "Failed to insert storage update into trie for key {key:#x} & value {value:#x}"
-        ));
+            .insert(IDENTIFIER, &key, &value)
+            .expect("Failed to insert into Bonsai storage")
     }
 
-    let id = id_builder.new_id();
+    let mut id_builder = BasicIdBuilder::new();
     bonsai_storage
-        .commit(id)
-        .expect("Failed to commit to bonsai storage");
-    let root_hash = bonsai_storage.root_hash().expect("Failed to get root hash");
-
-    println!("root hash: {root_hash:#x}");
+        .commit(id_builder.new_id())
+        .expect("Failed to commit to Bonsai storage");
+    bonsai_storage
+        .root_hash(IDENTIFIER)
+        .expect("Failed to retrieve root hash")
 }
